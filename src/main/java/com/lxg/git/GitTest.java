74989ae93356db6bd5896bf2b3c3364610a87826
luxiaogen <1986854537@qq.com>
2022-03-27 11:10:27 +0800

    first commit

---
+++ src/main/java/com/lxg/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.lxg.git;
+
+/**
+ * @author 陆小根
+ * date: 2022/03/27
+ * Description:
+ */
+
+public class GitTest {
+  public static void main(String[] args) {
+    System.out.println("hello git!");
+  }
+}
